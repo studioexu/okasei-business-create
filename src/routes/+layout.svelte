d884da698a2330623fe92678b0ae74c1444119d9
MasamiOgasawara <m-ogasawara@wely.tech>
2023-09-23 16:40:46 +0700

    feature: ヘッダーを読み込み、CSSを設定

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,35 @@
 <script lang="ts" context="module">
+	import { page } from '$app/stores'
 	import Header from '@/views/Header.svelte'
 	import Sidebar from '@/views/Sidebar.svelte'
 </script>
 
-<script lang="ts"></script>
+<script lang="ts">
+	const path: string | null = $page.route.id
+	const titles: Record<string, string> = {
+		users: '社員一覧',
+		customers: '顧客管理',
+		purchases: '買取一覧',
+		negotiations: '商談一覧'
+	}
 
-<Header pageTitle="aaa" />
-<Sidebar />
+	const slicedPath = path ? path.slice(1) : path
+	const title = slicedPath && titles.hasOwnProperty(slicedPath) ? titles[slicedPath] : ''
+</script>
+
+{#if path !== '/' && path !== null}
+	<Header {title} authority="admin" id="000000" name="山田太郎" />
+	<Sidebar />
+{/if}
+<slot />
 
 <style>
+	:root {
+		--primary-color: #0093d0;
+		--background-color: #b7eaff;
+		--text-color: #383c3c;
+	}
+
 	:global(*) {
 		margin: 0;
 		padding: 0;
@@ -22,11 +43,18 @@
 	}
 
 	:global(body) {
+		background: var(--background-color);
 		overscroll-behavior-x: none;
 	}
 
-  :global(.large) {
-    font-size: 21px;
-    font-weight: bold;
-  }
+	:global(span),
+	:global(p) {
+		font-size: 18px;
+		color: var(--text-color);
+	}
+
+	:global(.font-large) {
+		font-size: 21px;
+		font-weight: bold;
+	}
 </style>

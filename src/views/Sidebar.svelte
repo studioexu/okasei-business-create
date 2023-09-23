595e96b2944c8b48c840953dc01edee9ec736942
MasamiOgasawara <m-ogasawara@wely.tech>
2023-09-23 18:11:25 +0700

    feature: サイドバーを作成

--- src/views/Sidebar.svelte
+++ src/views/Sidebar.svelte
@@ -1,32 +1,99 @@
-<script lang="ts" context="module"></script>
+<script lang="ts" context="module">
+	import Icon from '@/components/Icon.svelte'
+</script>
 
-<script lang="ts"></script>
+<script lang="ts">
+	export let path: string
 
-<nav class="navbar">
+	const menus: { path: string; text: string }[] = [
+		{ path: 'users', text: '社員一覧' },
+		{ path: 'customers', text: '顧客管理' },
+		{ path: 'purchases', text: '買取一覧' },
+		{ path: 'negotiations', text: '商談一覧' },
+		{ path: 'history', text: '変更履歴' },
+		{ path: 'settings', text: '設定' }
+	]
+</script>
+
+<nav class="nav">
 	<ul class="nav-menu">
-		<li>
-			<details>
-				<summary><a>ユーザー一覧</a></summary>
-				<ul>
-					<li><a>新規登録</a></li>
-					<li><a>編集</a></li>
-				</ul>
-			</details>
-		</li>
-		<li>
-			<details>
-				<summary><a>顧客管理</a></summary>
-				<ul>
-					<li><a>新規登録</a></li>
-					<li><a>編集</a></li>
-				</ul>
-			</details>
-		</li>
-		<li><a>買取一覧</a></li>
-		<li><a>商談一覧</a></li>
+		{#each menus as menu}
+			<li class:active={`/${menu.path}` === path}>
+				<a href={`/${menu.path}`}>
+					<span class:invisible={`/${menu.path}` !== path}>
+						<Icon icon={{ path: `icons/${menu.path}`, color: '#0093d0' }} />
+					</span>
+					<span class:invisible={`/${menu.path}` === path}>
+						<Icon icon={{ path: `icons/${menu.path}` }} />
+					</span>
+					{menu.text}
+				</a>
+			</li>
+		{/each}
 	</ul>
+	<span class="logout">
+		<a href="/"><Icon icon={{ path: `icons/logout` }} />ログアウト</a>
+	</span>
 </nav>
 
 <style lang="scss">
-  
+	.nav {
+		position: relative;
+		display: inline-block;
+		height: calc(100vh - 64px);
+		width: 200px;
+		background: var(--primary-color);
+		padding-top: 16px;
+
+		> .nav-menu {
+			list-style: none;
+
+			> li {
+				color: #fff;
+				padding: 8px 24px;
+				margin-bottom: 16px;
+
+				&:last-of-type {
+					margin-bottom: 0px;
+				}
+			}
+
+			.active {
+				background: #fff;
+				color: var(--primary-color);
+			}
+
+			.invisible {
+				display: none;
+
+				// > :global(.svg-icon) {
+				// 	width: 0;
+				// 	height: 0;
+				// 	visibility: hidden;
+				// }
+			}
+		}
+
+		.logout {
+			position: absolute;
+			bottom: 64px;
+			color: #fff;
+			padding: 0 24px;
+		}
+
+		a {
+			display: flex;
+			align-items: center;
+
+			> span {
+				display: flex;
+				align-items: center;
+			}
+
+			> span,
+			> :global(.svg-icon) {
+				margin-right: 8px;
+			}
+		}
+	}
 </style>

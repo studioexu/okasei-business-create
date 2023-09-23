022dba84185157a02a69bf687b3bcc49202f0724
MasamiOgasawara <m-ogasawara@wely.tech>
2023-09-23 18:10:56 +0700

    feature: アイコンを表示するコンポーネントを作成

---
+++ src/components/Icon.svelte
@@ -0,0 +1,42 @@
+<script lang="ts" context="module">
+	import { onMount } from 'svelte'
+
+	const loadSvg = ({ path, color }: { path: string; color?: string }): Promise<HTMLElement> => {
+		try {
+			return fetch(`${path}.svg`)
+				.then(res => res.text())
+				.then(text => {
+					const svg = new DOMParser().parseFromString(text, 'image/svg+xml').documentElement
+					svg.classList.add('svg-icon')
+					svg.setAttribute('fill', color ?? '#fff')
+
+					return svg
+				})
+		} catch (error) {
+			throw Error(<string>error)
+		}
+	}
+</script>
+
+<script lang="ts">
+	export let icon: { path: string; color?: string }
+
+	let svg: HTMLElement
+
+	onMount(() =>
+		loadSvg(icon).then(result => {
+			if (result) svg = result
+		})
+	)
+</script>
+
+{#if svg}
+	{@html svg.outerHTML}
+{/if}
+
+<style>
+	:global(.svg-icon) {
+		display: inline-block;
+		height: 18px * 1.2;
+	}
+</style>

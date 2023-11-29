13a6ff0fecddf45523462e77649cf6b6679e5067
wfarre <william.farre@gmail.com>
2023-11-29 20:06:28 +0800

    update: バックエンドと結合（GETメソード）

--- src/data/api.js
+++ src/data/api.js
@@ -4,4 +4,4 @@ const newAPI = 'http://18.182.209.227/customer/list/customer'
 
 export const currentApi = newAPI
 
-export const currentKey = '99d1e45263c6f0853035869f8d94483646d55efa'
+export const currentKey = ''

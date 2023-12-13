682c382e6a8a75d49c7a247eb7bcc5d1e57882c2
wfarre <william.farre@gmail.com>
2023-12-13 10:26:50 +0800

    test API

--- src/data/api.ts
+++ src/data/api.ts
@@ -2,8 +2,8 @@ const localApi = 'http://localhost:3000'
 const demoApi = 'https://fake-server-2x0o.onrender.com/customers/'
 const newAPI = 'http://18.182.209.227'
 
-const newDjangoApi = 'https://test.djangoapi.xyz/'
+const newDjangoApi = 'https://test.okaseiexu.net/'
 
 export const currentApi: string = newDjangoApi
 
-export let currentKey: string = 'd4baca5afb33e91d6031074510fffc2b6a9de86a'
+export let currentKey: string = '0cc6b101fb3babae421809b0d8933dc104b7de3f'

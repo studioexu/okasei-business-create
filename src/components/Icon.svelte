d97a241605d447eadb1711d91b16857c1f4844e6
MasamiOgasawara <m-ogasawara@wely.tech>
2023-12-19 14:14:37 +0700

    refactoring: add async and calc in css

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -1,29 +1,31 @@
 <script lang="ts" context="module">
 	import { onMount } from 'svelte'
 
-	const loadSvg = ({ path, color }: { path: string; color: string }): Promise<HTMLElement> => {
+	const loadSvg = async ({
+		path,
+		color
+	}: {
+		path: string
+		color: string
+	}): Promise<HTMLElement> => {
 		try {
-			return fetch(`icons/${path}.svg`)
-				.then(res => res.text())
-				.then(text => {
-					const svg = new DOMParser().parseFromString(text, 'image/svg+xml').documentElement
-					svg.classList.add('svg-icon')
+			const res = await fetch(`icons/${path}.svg`)
+			const text = await res.text()
+			const svg = new DOMParser().parseFromString(text, 'image/svg+xml').documentElement
+			svg.classList.add('svg-icon')
+			switch (color) {
+				case 'black':
+					svg.setAttribute('fill', '#595857')
+					break
 
-					switch (color) {
-						case 'black':
-							svg.setAttribute('fill', '#595857')
-							break
+				case 'primary':
+					svg.setAttribute('fill', '#0093d0')
+					break
 
-						case 'primary':
-							svg.setAttribute('fill', '#0093d0')
-							break
-
-						default:
-							svg.setAttribute('fill', color)
-					}
-
-					return svg
-				})
+				default:
+					svg.setAttribute('fill', color)
+			}
+			return svg
 		} catch (error) {
 			throw Error(<string>error)
 		}
@@ -49,6 +51,6 @@
 <style>
 	:global(.svg-icon) {
 		display: inline-block;
-		height: 18px * 1.2;
+		height: calc(18px * 1.2);
 	}
 </style>

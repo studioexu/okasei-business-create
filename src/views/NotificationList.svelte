264852d571e22e917967ffe361226ba03dd14ad4
MasamiOgasawara <m-ogasawara@wely.tech>
2023-12-22 17:13:13 +0700

    feature: create a new component

---
+++ src/views/NotificationList.svelte
@@ -0,0 +1,24 @@
+<script lang="ts" context="module">
+	import type { Notifications } from '@/libs/notificationTypes'
+</script>
+
+<script lang="ts">
+	export let notifications: Notifications
+</script>
+
+<div class="notification-list">
+	{#each notifications as notification}
+		{notification}
+	{/each}
+</div>
+
+<style lang="scss">
+	.notification-list {
+		position: fixed;
+		right: 0;
+		height: 100%;
+		width: 320px;
+		background: #fff;
+		z-index: 10;
+	}
+</style>

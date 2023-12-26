37cb23efc641dd8e35d1ea35079afe7461038515
MasamiOgasawara <m-ogasawara@wely.tech>
2023-12-26 09:27:52 +0700

    feature: edit the component

--- src/views/NotificationList.svelte
+++ src/views/NotificationList.svelte
@@ -1,14 +1,17 @@
 <script lang="ts" context="module">
-	import type { Notifications } from '@/libs/notificationTypes'
+	import type { Notification } from '@/libs/notificationTypes'
 </script>
 
 <script lang="ts">
-	export let notifications: Notifications
+	export let notifications: Notification[]
 </script>
 
 <div class="notification-list">
 	{#each notifications as notification}
-		{notification}
+		<div class:unread={!notification.hasRead}>
+			<p>{notification.title}</p>
+			<p>{notification.content}</p>
+		</div>
 	{/each}
 </div>
 
@@ -16,9 +19,30 @@
 	.notification-list {
 		position: fixed;
 		right: 0;
-		height: 100%;
+		height: calc(100vh - 64px);
 		width: 320px;
 		background: #fff;
+		overflow-y: overlay;
 		z-index: 10;
+
+		> div {
+			width: 100%;
+			padding: 16px;
+			border-bottom: 1px solid var(--black);
+
+			&.unread {
+				background: #ddd;
+			}
+
+			> p {
+				line-height: 120%;
+				overflow-wrap: break-word;
+
+				&:first-child {
+					font-weight: bold;
+					margin-bottom: 8px;
+				}
+			}
+		}
 	}
 </style>
